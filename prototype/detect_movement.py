29dcee7f02f64360a68f767b7cdb4fb605723dbd
Sophie <sophie.zihan.perrot@gmail.com>
2026-03-02 11:17:04 +0000

    added update function to process hysteresis

--- prototype/detect_movement.py
+++ prototype/detect_movement.py
@@ -7,13 +7,14 @@ import time
 sensor = mpu6050(0x68)
 
 DATA_THRESHOLD = 8.0 # g but accounted for tilting
+CONFIDENCE_THRESHOLD = 10 # must be in new state for this num of cycles to update
 ORIENTATION = {"TOP": 1, "BOTTOM": 2, "LEFT": 3, 
                     "RIGHT": 4, "FRONT": 5, "BACK": 6}
 
 
 class OrientationDetector:
 	def __init__(self):
-		self.current_confirmed_state = "UNKNOWN"
+		self.current_state = "UNKNOWN"
 		self.pending_state = 0
 		self.confidence_counter = 0
 	
@@ -30,6 +31,28 @@ class OrientationDetector:
 		if y < -DATA_THRESHOLD: return ORIENTATION["BACK"]
 	
 		return None
+	
+	def update(self):
+		"""Returns True if state is officially changed (processes hysteresis)"""
+		reading = self.get_orientation()
+
+		if reading == self.current_state or reading is None:
+			self.confidence_counter = 0
+			return False
+		
+		if reading == self.pending_state:
+			self.confidence_counter += 1
+		else:
+			self.pending_state = reading
+			self.confidence_counter = 1
+		
+		if self.confidence_counter >= CONFIDENCE_THRESHOLD:
+			self.current_state = self.pending_state
+			self.confidence_counter = 0
+			return True
+		
+		return False
+
 
 
 if __name__ == "__main__":
@@ -37,7 +60,7 @@ if __name__ == "__main__":
 	try:
 		while True:
 			if detector.update():
-				print(f"Stable state: {detector.current_confirmed_state}")
+				print(f"Stable state: {detector.current_state}")
 			time.sleep(0.05) # 20 Hz sampling freq
 	except KeyboardInterrupt:
 		print("Orientation detection program ending")
